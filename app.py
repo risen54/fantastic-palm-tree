ae2c0ad3b4362b4cfd7cfcba9738ff3254f37e31
Satvik <90170287+Risen54@users.noreply.github.com>
2021-11-11 09:44:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ import os
 
 # voice setup
 engine = pyttsx3.init('sapi5')
-engine.setProperty('rate',205)  #speed of speech
-engine.setProperty('volume', 1)  #volume low(0) and high(1) 
+engine.setProperty('rate', 205)  # speed of speech
+engine.setProperty('volume', 1)  # volume low(0) and high(1)
 engine.setProperty('voice', 'en')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
@@ -30,9 +30,7 @@ pronoun = random.choice(pronouns)
 # Paths
 google_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
 vsc_path = r'C:\Users\admin\AppData\Local\Programs\Microsoft VS Code\Code.exe'
-yt_path = r'https://www.youtube.com'
-github_path = r'https://www.github.com/Risen54'
-
+yt_path = r'https:\\www.youtube.com'
 
 
 # Funtion to let the bot speak
@@ -61,109 +59,84 @@ def greet():  # Makes the bot greet you according to the time
         speak(greeting)
 
 
-def take_command():  # Receives and evaluates the given command
-    while True:
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print('Listening...')
-            r.adjust_for_ambient_noise(source, duration=1)
-            r.pause_threshold = 0.8
-            audio = r.listen(source)
-            
-            print('recognizing')
-            query = r.recognize_google(audio, language='en-in').lower()
-            print(f'Command: {query}\n')
-            
-            # Functions
-            # Add Emotions!!!!!!!!!!!!! 🤖
-            try:
+def take_command():
+    # takes microphone input from the user and returns string output
 
-                if 'open youtube' in query:
-                    webbrowser.WindowsDefault().open(yt_path)
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source, duration=1)
+        r.pause_threshold = 1
+        audio = r.listen(source)
 
-                elif 'open google' in query:
-                    subprocess.run(google_path)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
 
-                elif 'exit' in query or 'close' in query or 'quit' in query:
-                    exit()
-
-                elif 'stats' in query \
-                or 'health' in query \
-                or 'statistics' in query:
-                    heat = CPUTemperature()
-                    usage = int(psutil.cpu_percent())
-                    stats = f"""your cpu is running at {heat} \
-                    with {usage} percent of the cpu being utilized"""
-                    print(stats)
-                    speak(stats)
-
-                elif 'wikipedia' in query:
-                    speak("Searching...")
-                    query = query.replace("wikipedia", "")
-                    results = wikipedia.summary(query, sentences=3)
-                    speak("According  to Wikipedia...")
-                    print(results)
-                    speak(results)
-
-                elif 'open main folder' in query \
-                or 'open blizzard' in query \
-                or 'open my main folder' in query \
-                or 'open my folder' in query:
-
-                    subprocess.Popen(r'explorer /open,"G:\Blizzard\"')
-
-                elif 'open code' in query or \
-                'open visual studio code' in query:
-                    speak("Opening VSC")
-                    subprocess.run(vsc_path)
-
-                elif 'introduce yourself' in query\
-                or 'what can you do':
-                    intro = """
-                    I am you personal voice assistant.
-                    I can help you with stuff like reminding your tasks,
-                    give information about something,
-                    helping you with small things and
-                    making your work fast"""
-                    print(intro)
-                    speak(intro)
-
-                # Tell time function
-                elif 'the time' in query:
-                    time = datetime.datetime.now().strftime("%H:%M:%S")
-                    print(time)
-                    speak(time)
-                
-                # Tell date function
-                elif 'the date' in query:
-                    date = datetime.datetime.now().strftime("%d/%m/%Y")
-                    print(date)
-                    speak(date)
-                
-                # Tell day function
-                elif 'the day' in query:
-                    day = datetime.datetime.now().strftime("%A")
-                    print(day)
-                    speak(day)
-                
-                # Flip coin function
-                elif 'flip coin' in query:
-                    flip = random.choice(['heads', 'tails'])
-                    print(flip)
-                    speak(flip)
-
-                # Open Github function
-                elif 'open github' in query:
-                    webbrowser.WindowsDefault().open(github_path)
-
-                else:
-                    continue
-            except Exception:
-                print("Please say again")
-                return 'None'
-            return query
+    except Exception as e:
+        print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
 
 
 if __name__ == '__main__':
     greet()
-    take_command()
+    while True:
+        query = take_command().lower()
+
+        if 'carl' in query or 'car' in query:
+            if 'open youtube' in query:
+                webbrowser.WindowsDefault().open(yt_path)
+
+            elif 'open google' in query:
+                subprocess.run(google_path)
+
+            elif 'exit' in query \
+            or 'close' in query \
+            or 'quit' in query:
+                exit()
+
+            elif 'stats' in query \
+            or 'health' in query \
+            or 'statistics' in query:
+                heat = CPUTemperature()
+                usage = int(psutil.cpu_percent())
+                stats = f"""your cpu is running at {heat} \
+                with {usage} percent of the cpu being utilized"""
+                print(stats)
+                speak(stats)
+
+            elif 'wikipedia' in query:
+                speak("Searching...")
+                query = query.replace("wikipedia", "")
+                results = wikipedia.summary(query, sentences=3)
+                speak("According  to Wikipedia...")
+                print(results)
+                speak(results)
+
+            elif 'open main folder' in query \
+            or 'open blizzard' in query \
+            or 'open my main folder' in query \
+            or 'open my folder' in query:
+                subprocess.Popen(r'explorer /open,"G:\Blizzard\"')
+
+            elif 'open code' in query \
+            or 'open visual studio code' in query:
+                speak("Opening VSC")
+                subprocess.run(vsc_path)
+
+            elif 'introduce yourself' in query\
+            or 'what can you do':
+                intro = """
+                I am you personal voice assistant.
+                I can help you with stuff like reminding your tasks,
+                give information about something,
+                helping you with small things and
+                making your work fast"""
+                print(intro)
+                speak(intro)
+            elif 'the time' in query:
+                strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+                speak(f"Sir, the time is {strTime}")
